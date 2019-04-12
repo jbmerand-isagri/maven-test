f02c1befb587d88051d1185108e8345a78f60b31
Jean-Baptiste Mérand <jbmerand@tutanota.com>
2019-04-12 12:37:35 +0200

    step 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
 	    System.out.println( "Hey! Premier projet Maven :)" );
+	    String asciiArt = FigletFont.convertOneLine("Jean-Baptiste");
+	    System.out.println(asciiArt);
     }
 }
